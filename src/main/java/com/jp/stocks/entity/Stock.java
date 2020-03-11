072de75ee7fef6e722f03df400546d4dde69ef50
Bharath Balmoori <bharath.balmoori@gmail.com>
2020-03-11 16:19:09 +0000

    Removed lomnok annotations

--- src/main/java/com/jp/stocks/entity/Stock.java
+++ src/main/java/com/jp/stocks/entity/Stock.java
@@ -1,16 +1,79 @@
 package com.jp.stocks.entity;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NonNull;
 
-@Data
-@EqualsAndHashCode(callSuper = false)
 public class Stock extends Instrument {
-    @NonNull //ticker cannot be null
     private final String ticker;
     private final StockType stockType;
     private final double lastDividend;
     private final double fixedDividend;
     private final int parValue;
+
+    public Stock(String ticker, StockType stockType, double lastDividend, double fixedDividend, int parValue) {
+        this.ticker = ticker;
+        this.stockType = stockType;
+        this.lastDividend = lastDividend;
+        this.fixedDividend = fixedDividend;
+        this.parValue = parValue;
+    }
+
+    public String getTicker() {
+        return this.ticker;
+    }
+
+    public StockType getStockType() {
+        return this.stockType;
+    }
+
+    public double getLastDividend() {
+        return this.lastDividend;
+    }
+
+    public double getFixedDividend() {
+        return this.fixedDividend;
+    }
+
+    public int getParValue() {
+        return this.parValue;
+    }
+
+    public String toString() {
+        return "Stock(ticker=" + this.getTicker() + ", stockType=" + this.getStockType() + ", lastDividend=" + this.getLastDividend() + ", fixedDividend=" + this.getFixedDividend() + ", parValue=" + this.getParValue() + ")";
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Stock)) return false;
+        final Stock other = (Stock) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$ticker = this.getTicker();
+        final Object other$ticker = other.getTicker();
+        if (this$ticker == null ? other$ticker != null : !this$ticker.equals(other$ticker)) return false;
+        final Object this$stockType = this.getStockType();
+        final Object other$stockType = other.getStockType();
+        if (this$stockType == null ? other$stockType != null : !this$stockType.equals(other$stockType)) return false;
+        if (Double.compare(this.getLastDividend(), other.getLastDividend()) != 0) return false;
+        if (Double.compare(this.getFixedDividend(), other.getFixedDividend()) != 0) return false;
+        if (this.getParValue() != other.getParValue()) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof Stock;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $ticker = this.getTicker();
+        result = result * PRIME + ($ticker == null ? 43 : $ticker.hashCode());
+        final Object $stockType = this.getStockType();
+        result = result * PRIME + ($stockType == null ? 43 : $stockType.hashCode());
+        final long $lastDividend = Double.doubleToLongBits(this.getLastDividend());
+        result = result * PRIME + (int) ($lastDividend >>> 32 ^ $lastDividend);
+        final long $fixedDividend = Double.doubleToLongBits(this.getFixedDividend());
+        result = result * PRIME + (int) ($fixedDividend >>> 32 ^ $fixedDividend);
+        result = result * PRIME + this.getParValue();
+        return result;
+    }
 }
--- src/main/java/com/jp/stocks/entity/Trade.java
+++ src/main/java/com/jp/stocks/entity/Trade.java
@@ -1,17 +1,87 @@
 package com.jp.stocks.entity;
 
-import lombok.Data;
 import lombok.NonNull;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-@Data
 public class Trade {
-    @NonNull
     private final String symbol;
     private final LocalDateTime tradeTime;
     private final int quantity;
     private final TradeLeg tradeLeg;
     private final BigDecimal price;
+
+    public Trade(String symbol, LocalDateTime tradeTime, int quantity, TradeLeg tradeLeg, BigDecimal price) {
+        this.symbol = symbol;
+        this.tradeTime = tradeTime;
+        this.quantity = quantity;
+        this.tradeLeg = tradeLeg;
+        this.price = price;
+    }
+
+
+    public  String getSymbol() {
+        return this.symbol;
+    }
+
+    public LocalDateTime getTradeTime() {
+        return this.tradeTime;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public TradeLeg getTradeLeg() {
+        return this.tradeLeg;
+    }
+
+    public BigDecimal getPrice() {
+        return this.price;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Trade)) return false;
+        final Trade other = (Trade) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$symbol = this.getSymbol();
+        final Object other$symbol = other.getSymbol();
+        if (this$symbol == null ? other$symbol != null : !this$symbol.equals(other$symbol)) return false;
+        final Object this$tradeTime = this.getTradeTime();
+        final Object other$tradeTime = other.getTradeTime();
+        if (this$tradeTime == null ? other$tradeTime != null : !this$tradeTime.equals(other$tradeTime)) return false;
+        if (this.getQuantity() != other.getQuantity()) return false;
+        final Object this$tradeLeg = this.getTradeLeg();
+        final Object other$tradeLeg = other.getTradeLeg();
+        if (this$tradeLeg == null ? other$tradeLeg != null : !this$tradeLeg.equals(other$tradeLeg)) return false;
+        final Object this$price = this.getPrice();
+        final Object other$price = other.getPrice();
+        if (this$price == null ? other$price != null : !this$price.equals(other$price)) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof Trade;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $symbol = this.getSymbol();
+        result = result * PRIME + ($symbol == null ? 43 : $symbol.hashCode());
+        final Object $tradeTime = this.getTradeTime();
+        result = result * PRIME + ($tradeTime == null ? 43 : $tradeTime.hashCode());
+        result = result * PRIME + this.getQuantity();
+        final Object $tradeLeg = this.getTradeLeg();
+        result = result * PRIME + ($tradeLeg == null ? 43 : $tradeLeg.hashCode());
+        final Object $price = this.getPrice();
+        result = result * PRIME + ($price == null ? 43 : $price.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "Trade(symbol=" + this.getSymbol() + ", tradeTime=" + this.getTradeTime() + ", quantity=" + this.getQuantity() + ", tradeLeg=" + this.getTradeLeg() + ", price=" + this.getPrice() + ")";
+    }
 }
